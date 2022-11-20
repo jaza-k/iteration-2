734ee846d506424740e16bd806152e625a8f4574
Reid Erb <reid.edward.erb@gmail.com>
2022-11-19 23:55:19 -0700

    Delete com.diy.software.test directory

--- com.diy.software.test/src/com/diy/software/test/iteration1/AttendantDemo.java
+++
@@ -1,9 +0,0 @@
-package com.diy.software.test.iteration1;
-
-import com.diy.software.gui.AttendantGUI;
-
-public class AttendantDemo {
-	public static void main(String[] args) {
-    	AttendantGUI Attendant = new AttendantGUI();
-    }
-}
