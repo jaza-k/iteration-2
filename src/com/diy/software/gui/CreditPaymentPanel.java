07fe5a00184b028d3dced787fee595e585fc742b
Nyzean <paul.serafini1@ucalgary.ca>
2022-11-23 13:41:32 -0700

    fixing this branch

---
+++ src/com/diy/software/gui/CreditPaymentPanel.java
@@ -0,0 +1,92 @@
+package com.diy.software.gui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import com.diy.hardware.DoItYourselfStationAR;
+import com.diy.hardware.external.CardIssuer;
+import com.diy.simulation.Customer;
+import com.diy.software.payment.CreditPayment;
+import com.jimmyselectronics.disenchantment.TouchScreen;
+import com.jimmyselectronics.opeechee.Card;
+import com.jimmyselectronics.opeechee.InvalidPINException;
+
+public class CreditPaymentPanel extends JPanel {
+	
+	public CreditPaymentPanel(Customer customer, DoItYourselfStationAR doItYourselfStation, CardIssuer bank, TouchScreen screen, Cart cart) {
+		
+		JComboBox<String> cardComboBox = new JComboBox<String>();
+		for (Card card : customer.wallet.cards) {
+			cardComboBox.addItem(card.cardholder + ":" + card.kind);
+		}
+		//Add customer cards to the combo box here
+		cardComboBox.setBounds(109, 446, 91, 23);
+		add(cardComboBox);
+		
+		
+	
+	
+			JLabel Wallet = new JLabel("Cards in Wallet");
+			Wallet.setForeground(Color.DARK_GRAY);
+			Wallet.setFont(new Font("Georgia", Font.PLAIN, 13));
+			Wallet.setHorizontalAlignment(SwingConstants.CENTER);
+			Wallet.setBounds(109, 434, 91, 11);
+			add(Wallet);
+	
+		
+		JLabel pinLabel = new JLabel("Pin:");
+		pinLabel.setBounds(247, 470, 60, 23);
+		add(pinLabel);
+		JTextField pinField = new JTextField();
+		pinField.setBounds(277, 470, 40, 23);
+		add(pinField);
+		JButton PayButton = new JButton("Payment");
+		PayButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Action when "pay" button clicked
+				try {
+					
+					CreditPayment newpay = new CreditPayment();
+					newpay.setCard(customer.wallet.cards.get(cardComboBox.getSelectedIndex()));
+					newpay.setReader(doItYourselfStation.cardReader);
+					newpay.setCardIssuer(bank);
+					//System.out.println("Attempting to insert card.");
+					newpay.insertCard(pinField.getText().intern()); //The intern() function will make sure the string is properly formatted.
+					//System.out.println("Card successfully inserted");
+					boolean flag = newpay.payForTotal(cart.addItemScanned.getTotal());
+					if (flag)
+					{
+						screen.setVisible(false);
+					}
+					//else System.out.println("Transaction failed");
+					
+				}
+				catch (InvalidPINException e2)
+				{
+					JOptionPane.showMessageDialog(getParent(), "Invalid Transaction!", "Payment Error", JOptionPane.ERROR_MESSAGE);
+					//System.out.println(e2.toString());
+				}
+				catch (IOException e1) {
+					// When Transcation Fails
+					JOptionPane.showMessageDialog(getParent(), "Invalid Transaction!", "Payment Error", JOptionPane.ERROR_MESSAGE);
+					//System.out.println(e1.toString());
+				}
+			}
+		});
+		PayButton.setFont(new Font("Georgia", Font.PLAIN, 13));
+		PayButton.setBounds(237, 445, 91, 23);
+		add(PayButton);
+	}
+	
+}
--- src/com/diy/software/gui/GUI.java
+++ src/com/diy/software/gui/GUI.java
@@ -2,33 +2,68 @@ package com.diy.software.gui;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
 
 import com.diy.hardware.DoItYourselfStationAR;
 import com.diy.hardware.external.CardIssuer;
+import com.diy.hardware.external.ProductDatabases;
+import com.jimmyselectronics.disenchantment.TouchScreen;
 import com.diy.simulation.Customer;
+import com.diy.software.payment.CashPaymentPanel;
+import com.diy.software.payment.DebitPaymentPanel;
+import com.diy.software.scansoft.AddItemScanned;
+
 
 public class GUI{
 	private JFrame frame;
 	private CustomJPanel panel;
+	private PayPanel payPanel;
+	private JTabbedPane tabbedPane;
+	private CreditPaymentPanel creditPanel;
+	private CashPaymentPanel cashPanel;
+	private DebitPaymentPanel debitPanel;
 	/**
 	 * 
 	 * @param doItYourselfStation: The current station
 	 * @param wallet: Requires the wallet so the cards can be accessed and displayed onto the GUI
 	 * @param curretCart: Requires all the items as an array list;items can be displayed to simulate a scan
+	 * @param screen: Contains the code to create the frame.
 	 */
-	/*
-	public GUI(DoItYourselfStationAR doItYourselfStation, Customer customer, CardIssuer bank) {
-		frame = doItYourselfStation.touchScreen.getFrame();
+	public GUI(DoItYourselfStationAR doItYourselfStation, Customer customer, CardIssuer bank, TouchScreen screen) {
 		
-		panel = new CustomJPanel(customer, doItYourselfStation, bank);
+		AddItemScanned addItemScanned = new AddItemScanned(ProductDatabases.BARCODED_PRODUCT_DATABASE, doItYourselfStation.scanner);
+		Cart cart = new Cart(addItemScanned);
+		
+		
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		panel = new CustomJPanel(customer, doItYourselfStation, bank, screen, cart, tabbedPane);
+		payPanel = new PayPanel(tabbedPane);
+		creditPanel = new CreditPaymentPanel(customer, doItYourselfStation, bank, screen, cart);
+		cashPanel = new CashPaymentPanel(customer, doItYourselfStation, bank, screen, cart);
+		debitPanel = new DebitPaymentPanel(customer, doItYourselfStation, bank, screen, cart);
+		
+		
+		
+		tabbedPane.add(panel);
+		tabbedPane.add(payPanel);
+		tabbedPane.add(creditPanel);
+		tabbedPane.add(cashPanel);
+		tabbedPane.add(debitPanel);
+	    tabbedPane.setEnabledAt(1, false);
+	    tabbedPane.setEnabledAt(0, false);
+	    tabbedPane.setEnabledAt(2, false);
+	    tabbedPane.setEnabledAt(3, false);
+	    tabbedPane.setEnabledAt(4, false);
+	    
+	    
+		frame = screen.getFrame();
 		frame.setTitle("Customer GUI");
-		
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		frame.pack();
 		frame.setSize(426, 500);
 		frame.setLocationRelativeTo(null);
-		doItYourselfStation.touchScreen.setVisible(true);
+		screen.setVisible(true);
 	}
 
-	 */
+
 }
