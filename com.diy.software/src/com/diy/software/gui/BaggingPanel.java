81e5f942981c4c95e3247270bf6996a9d70bf4f1
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2022-11-23 15:00:31 -0700

    Add Bagging Panel to Customer GUI

---
+++ com.diy.software/src/com/diy/software/gui/BaggingPanel.java
@@ -0,0 +1,63 @@
+package com.diy.software.gui;
+
+import javax.swing.JPanel;
+
+import com.diy.hardware.DoItYourselfStationAR;
+import com.diy.hardware.external.CardIssuer;
+import com.diy.simulation.Customer;
+import com.jimmyselectronics.disenchantment.TouchScreen;
+import javax.swing.JButton;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.SystemColor;
+import javax.swing.JTextPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class BaggingPanel extends JPanel {
+	private JTextField numberOfBags;
+
+	/**
+	 * Create the panel.
+	 */
+	public BaggingPanel(Customer customer, TouchScreen screen, Cart cart, JTabbedPane tabbedPane) {
+		setBackground(SystemColor.inactiveCaption);
+		setLayout(null);
+		
+		JButton btnNewButton = new JButton("Continue to Payment");
+		btnNewButton.setFont(new Font("Georgia", Font.PLAIN, 13));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tabbedPane.setSelectedIndex(1);
+			}
+		});
+		btnNewButton.setBounds(203, 384, 189, 39);
+		add(btnNewButton);
+		
+		JTextPane scannedItemPane = new JTextPane();
+		scannedItemPane.setEditable(false);
+		scannedItemPane.setBounds(63, 82, 329, 98);
+		add(scannedItemPane);
+		
+		JButton buyBag = new JButton("Buy Bags");
+		buyBag.setBounds(267, 224, 114, 23);
+		add(buyBag);
+		
+		numberOfBags = new JTextField();
+		numberOfBags.setBounds(95, 225, 106, 20);
+		add(numberOfBags);
+		numberOfBags.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Number of Bags");
+		lblNewLabel.setFont(new Font("Georgia", Font.PLAIN, 13));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(84, 209, 122, 14);
+		add(lblNewLabel);
+
+	}
+}
