9f7c8f1523f65c96acb2d9a35cd47a60e921d92e
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2022-11-24 04:16:35 -0700

    Update Discrepancy Panel [visually] for Customer I/O

--- src/com/diy/software/gui/UnexpectedWeight.java
+++ src/com/diy/software/gui/UnexpectedWeight.java
@@ -18,15 +18,20 @@ public class UnexpectedWeight extends JPanel {
 		setBackground(new Color(240, 128, 128));
 		setLayout(null);
 		
-		JTextArea txtrAnUnexpectedWeight = new JTextArea();
-		txtrAnUnexpectedWeight.setBackground(new Color(192, 192, 192));
-		txtrAnUnexpectedWeight.setWrapStyleWord(true);
-		txtrAnUnexpectedWeight.setFont(new Font("Monospaced", Font.PLAIN, 14));
-		txtrAnUnexpectedWeight.setLineWrap(true);
-		txtrAnUnexpectedWeight.setText("An unexpected weight was detected! Please wait for assistance.");
-		txtrAnUnexpectedWeight.setEditable(false);
-		txtrAnUnexpectedWeight.setBounds(99, 150, 245, 199);
-		add(txtrAnUnexpectedWeight);
+		JLabel lblNewLabel = new JLabel("Please wait for assistance!");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Georgia", Font.PLAIN, 15));
+		lblNewLabel.setBackground(new Color(255, 255, 255));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(126, 181, 221, 161);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Station Blocked!");
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setFont(new Font("Georgia", Font.PLAIN, 14));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(116, 216, 221, 17);
+		add(lblNewLabel_1);
 
 	}
 }
