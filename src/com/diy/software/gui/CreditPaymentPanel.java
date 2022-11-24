e54531ad3db89c9a289946d22aa8f2bc62e5bb4d
Abhay Chopra <69493925+Abhay-Chopra@users.noreply.github.com>
2022-11-23 20:24:01 -0700

    Add Payment Panel to Customer GUI

--- src/com/diy/software/gui/CreditPaymentPanel.java
+++ src/com/diy/software/gui/CreditPaymentPanel.java
@@ -2,9 +2,11 @@ package com.diy.software.gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -13,9 +15,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 
-import com.diy.hardware.DoItYourselfStationAR;
 import com.diy.hardware.external.CardIssuer;
 import com.diy.simulation.Customer;
 import com.diy.software.DoItYourselfStationLogic;
@@ -26,80 +28,74 @@ import com.jimmyselectronics.opeechee.InvalidPINException;
 
 public class CreditPaymentPanel extends JPanel {
 	
-	public CreditPaymentPanel(Customer customer, DoItYourselfStationLogic stationLogic, JTabbedPane tabbedPane) {
+	public CreditPaymentPanel(Customer customer, DoItYourselfStationLogic stationLogic, JTabbedPane tabbedPane, TouchScreen screen,  HashMap<Card, CardIssuer> bankingInfo) {
+		setBackground(SystemColor.inactiveCaption);
 		
-		
-		/**
-		 * 
 		JComboBox<String> cardComboBox = new JComboBox<String>();
 		for (Card card : customer.wallet.cards) {
 			cardComboBox.addItem(card.cardholder + ":" + card.kind);
 		}
+		setLayout(null);
 		//Add customer cards to the combo box here
-		cardComboBox.setBounds(109, 446, 91, 23);
+		cardComboBox.setBounds(79, 370, 114, 22);
 		add(cardComboBox);
 		
-		
-	
-	
-			JLabel Wallet = new JLabel("Cards in Wallet");
-			Wallet.setForeground(Color.DARK_GRAY);
-			Wallet.setFont(new Font("Georgia", Font.PLAIN, 13));
-			Wallet.setHorizontalAlignment(SwingConstants.CENTER);
-			Wallet.setBounds(109, 434, 91, 11);
-			add(Wallet);
-	
+		JLabel Wallet = new JLabel("Cards in Wallet");
+		Wallet.setForeground(Color.DARK_GRAY);
+		Wallet.setFont(new Font("Georgia", Font.PLAIN, 13));
+		Wallet.setHorizontalAlignment(SwingConstants.CENTER);
+		Wallet.setBounds(69, 355, 124, 15);
+		add(Wallet);
 		
 		JLabel pinLabel = new JLabel("Pin:");
-		pinLabel.setBounds(247, 470, 60, 23);
+		pinLabel.setFont(new Font("Georgia", Font.PLAIN, 13));
+		pinLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		pinLabel.setBounds(257, 359, 43, 33);
 		add(pinLabel);
+		
 		JTextField pinField = new JTextField();
-		pinField.setBounds(277, 470, 40, 23);
+		pinField.setToolTipText("pin");
+		pinField.setBounds(298, 366, 86, 20);
 		add(pinField);
-		JButton PayButton = new JButton("Payment");
+		
+		JButton PayButton = new JButton("Make Payment");
+		PayButton.setBackground(new Color(143, 188, 143));
+		
 		PayButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// Action when "pay" button clicked
+				// Action when payment being attempted
 				try {
 					
 					CreditPayment newpay = new CreditPayment();
 					newpay.setCard(customer.wallet.cards.get(cardComboBox.getSelectedIndex()));
-					newpay.setReader(doItYourselfStation.cardReader);
-					newpay.setCardIssuer(bank);
-					//System.out.println("Attempting to insert card.");
-					newpay.insertCard(pinField.getText().intern()); //The intern() function will make sure the string is properly formatted.
-					//System.out.println("Card successfully inserted");
-					
-					
-					//boolean flag = newpay.payForTotal(cart.addItemScanned.getTotal());
-					///////////////////////////////
-					// BELOW BOOL IS PLACEHOLDER //
-					///////////////////////////////
-					boolean flag = true;	// temporary junk
+					newpay.setReader(stationLogic.station.cardReader);
+					newpay.setCardIssuer(bankingInfo.get(customer.wallet.cards.get(cardComboBox.getSelectedIndex())));
+					//The intern() function will make sure the string is properly formatted.
+					newpay.insertCard(pinField.getText().intern()); 
+					boolean flag = newpay.payForTotal(stationLogic.scannerController.getTotal());
 					
 					if (flag)
 					{
 						screen.setVisible(false);
 					}
-					//else System.out.println("Transaction failed");
-					
 				}
 				catch (InvalidPINException e2)
 				{
 					JOptionPane.showMessageDialog(getParent(), "Invalid Transaction!", "Payment Error", JOptionPane.ERROR_MESSAGE);
-					//System.out.println(e2.toString());
 				}
 				catch (IOException e1) {
 					// When Transaction Fails
 					JOptionPane.showMessageDialog(getParent(), "Invalid Transaction!", "Payment Error", JOptionPane.ERROR_MESSAGE);
-					//System.out.println(e1.toString());
 				}
 			}
 		});
 		PayButton.setFont(new Font("Georgia", Font.PLAIN, 13));
-		PayButton.setBounds(237, 445, 91, 23);
+		PayButton.setBounds(267, 403, 124, 49);
 		add(PayButton);
-	}
-	 */
+		
+		JTextPane scannedItemPane = new JTextPane();
+		scannedItemPane.setEditable(false);
+		scannedItemPane.setBounds(55, 22, 329, 304);
+		add(scannedItemPane);
 	}
 }
