7608a96deff5963fabe40cd39c2778ec97619afd
Reid Erb <reid.edward.erb@gmail.com>
2022-11-24 18:25:05 -0700

    Update CustomerTerminalDemo.java

--- src/com/diy/software/test/iteration1/CustomerTerminalDemo.java
+++ src/com/diy/software/test/iteration1/CustomerTerminalDemo.java
@@ -1,14 +1,4 @@
-package com.diy.software.test.iteration1;
 
-import com.diy.simulation.Customer;
-import com.diy.software.gui.CheckOutSystem;
-
-public class CustomerTerminalDemo {
-    public static void main(String[] args) {
-        Customer customer = new Customer();
-        CheckOutSystem system = new CheckOutSystem(customer);
-    }
-=======
 package com.diy.software.test.iteration1;
 
 import com.diy.simulation.Customer;
